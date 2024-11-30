3e4e759a3ee4be908d67bfe80f40995975484895
tArogoD <jfu5285@mysvc.skagit.edu>
2024-12-01 00:44:02 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -30,7 +30,7 @@ if ! kill -0 $DASHBOARD_PID 2>/dev/null; then
 fi
 
 # 启动 Cloudflare Tunnel
-nohup ./cloudflared-linux-amd64 tunnel --edge-ip-version auto --protocol http2 --no-autoupdate run --token "$ARGO_AUTH" &
+nohup ./cloudflared-linux-amd64 tunnel --edge-ip-version auto --protocol http2 run --token "$ARGO_AUTH" &
 
 # 启动 Nginx 并保持前台运行
 nginx -g "daemon off;"
