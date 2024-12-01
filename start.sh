af1cce3fe5d500141c9a4abd5aa8752365f85042
tArogoD <jfu5285@mysvc.skagit.edu>
2024-12-01 09:39:11 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 ARGO_AUTH=${ARGO_AUTH:-'eyJhIjoiZjAzMGY1ZDg4OGEyYmRlN2NiMDg3NTU5MzM4ZjE0OTciLCJ0IjoiZjIzMDA3MWUtYjBhMy00ZGM0LTg2MjYtMDVjYjQzNmM3Y2YxIiwicyI6Ik0yTTRPVEl6TWpjdE1tWXdPQzAwTWpkaUxUazJNamN0Tkdaak9EaGlNakE0T1dKayJ9'}
 
 # 下载并解压 Dashboard
@@ -13,8 +14,15 @@ if [ ! -f "cloudflared-linux-amd64" ]; then
   wget -q https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64
 fi
 
+# 下载 Nezha Agent
+if [ ! -f "nezha-agent_linux_amd64.zip" ]; then
+  wget -q https://github.com/nezhahq/agent/releases/latest/download/nezha-agent_linux_amd64.zip
+fi
+unzip -qo nezha-agent_linux_amd64.zip
+rm -f nezha-agent_linux_amd64.zip
+
 # 设置执行权限
-chmod +x dashboard-linux-amd64 cloudflared-linux-amd64
+chmod +x dashboard-linux-amd64 cloudflared-linux-amd64 nezha-agent
 
 # 启动 Dashboard 并重定向输出
 nohup ./dashboard-linux-amd64 &
@@ -31,6 +39,9 @@ fi
 
 # 启动 Cloudflare Tunnel
 nohup ./cloudflared-linux-amd64 tunnel --edge-ip-version auto --protocol http2 run --token "$ARGO_AUTH" &
+
+# 启动 Nezha Agent
+NZ_SERVER=localhost:443 NZ_TLS=false NZ_CLIENT_SECRET=$NZ_agentsecretkey nohup ./nezha-agent &
 
 # 启动 Nginx 并保持前台运行
 nginx -g "daemon off;"
