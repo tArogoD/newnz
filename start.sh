76e28aa2b370c5e87a46c130f83145e2f48b56ca
tArogoD <jfu5285@mysvc.skagit.edu>
2024-12-03 08:54:01 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -47,9 +47,6 @@ fi
 # Start Cloudflare Tunnel
 nohup ./cloudflared-linux-amd64 tunnel --edge-ip-version auto --protocol http2 run --token "$ARGO_AUTH" &
 
-OUTPUT=$(cloudflared tunnel info --token "$ARGO_AUTH" 2>&1)
-DOMAIN=$(echo "$OUTPUT" | grep -oP "(?<=domain: ).*")
-
 # Start Nezha Agent
 NZ_SERVER=$DOMAIN:443 NZ_TLS=true NZ_CLIENT_SECRET=$NZ_agentsecretkey nohup ./nezha-agent &
 
