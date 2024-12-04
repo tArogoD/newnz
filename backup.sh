680f06c3293d371b59177e595fc5aaad404cadb3
tArogoD <jfu5285@mysvc.skagit.edu>
2024-12-04 08:08:47 +0800

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -6,9 +6,9 @@ if [ -z "$GITHUB_USERNAME" ] || [ -z "$REPO_NAME" ] || [ -z "$GITHUB_TOKEN" ]; t
     exit 1
 fi
 
-# Create timestamp for backup
-TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
-BACKUP_FILE="data_backup_${TIMESTAMP}.tar.gz"
+# Create timestamp for backup (Shanghai time)
+TIMESTAMP=$(TZ='Asia/Shanghai' date +"%Y-%m-%d-%H:%M:%S")
+BACKUP_FILE="data-${TIMESTAMP}.tar.gz"
 
 # Compress data directory
 tar -czvf "$BACKUP_FILE" data/
@@ -23,17 +23,14 @@ fi
 
 cd temp_repo
 
-# Add backup file
-mkdir -p backups
-cp "../$BACKUP_FILE" backups/
+# Add backup file to repo root
+cp "../$BACKUP_FILE" ./
 
 # Remove old backups, keeping only the 5 most recent
-cd backups
-ls -t | tail -n +6 | xargs -I {} rm {}
-cd ..
+ls data-*.tar.gz | sort -r | tail -n +6 | xargs -I {} rm -f {}
 
 # Commit and push
-git add backups/"$BACKUP_FILE"
+git add "$BACKUP_FILE"
 git config user.name "Backup Script"
 git config user.email "backup@localhost"
 git commit -m "Add backup: $BACKUP_FILE"
